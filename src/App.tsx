d2157a25de4ecda04655a35deb8c5dc746e70a3a
payne3105 <payne3105@gmail.com>
2022-11-06 21:25:32 +0300

    react typescript tutorial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import Card, { CardVariant } from './components/Card';
+import UserList from './components/UserList';
+import { IUser } from './types/types';
+import axios from "axios";
+import List from "./components/List";
+import UserItem from "./components/UserItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState<IUser[]>([])
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    async function fetchUsers() {
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+            setUsers(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
+    return (
+        <div>
+            <Card width='200px' height='200px' variant={CardVariant.outlined} >
+                <button>Button</button>
+                <div>Text</div>
+            </Card>
+            <List items={users}
+                  renderItem={(user: IUser) => <UserItem user={user} key={user.id}/>}
+            />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/UserItem.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react'
+import {IUser} from "../types/types";
+
+interface UserItemProps {
+  user: IUser;
+}
+
+const UserItem: FC<UserItemProps> = ({user}) => {
+  return (
+      <div style={{padding: 15, border: '1px solid gray'}}>
+        {user.id}. {user.name} lives in the city {user.address.city} on the street {user.address.street}
+      </div>
+  )
+}
+
+export default UserItem
